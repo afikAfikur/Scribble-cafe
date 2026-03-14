1eff05864acd3195621e768042b950b7205ac56c
Afik <afikrahman28@gmail.com>
2026-03-14 22:50:13 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Navber from './components/navber'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+   <Navber></Navber>
+
+  
+
+
+
+    </>
+  )
+}
+
+export default App
---
+++ src/components/navber.jsx
@@ -0,0 +1,8 @@
+const Navber=()=>{
+ return (
+<div>
+    <h2 className="border">Javascript Vlogs</h2>
+</div>    
+ )
+}
+export default Navber
