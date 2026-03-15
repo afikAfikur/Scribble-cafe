d758a9c54d9be42633d039863715d62784099b83
Afik <afikrahman28@gmail.com>
2026-03-15 15:23:15 +0600

    done with navbar

--- src/components/navber.jsx
+++ src/components/navber.jsx
@@ -1,7 +1,9 @@
+import UserIcon from '../assets/icons8-user-100.png'
 const Navber=()=>{
  return (
-<div>
-    <h2 className="border">Javascript Vlogs</h2>
+<div className='flex items-center justify-between px-[100px] pt-[20px] border-b border-gray-200'>
+    <h2 className="font-bold text-blue-900 text-[32px]">Knowledge Cafe</h2>
+    <img className='' src={UserIcon} alt="" />
 </div>    
  )
 }
