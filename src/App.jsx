7ca7389b9bf6b74135a62b995da394e1dd27aa28
Afik <afikrahman28@gmail.com>
2026-03-16 19:54:43 +0600

    Done with author

--- src/App.jsx
+++ src/App.jsx
@@ -11,13 +11,13 @@ function App() {
       <Navber></Navber>
 
       <div className="Main-container  flex flex-col md:flex-row gap-[20px] px-5 md:px-20 lg:px-[100px] pt-4   md:pt-[20px]">
-        <div className="w-[100%] md:w-[70%] ">
-          <h2 className="mb-[30px]">Blogs Total:{blogCount}</h2>
+        <div className="w-[100%] md:w-[70%] p-10px">
+          <h2 className="mb-[30px] ml-[10px] font-bold text-[24px]">Blogs Total:{blogCount}</h2>
           <Blogs setBlogCount={setBlogCount}></Blogs>
         </div>
-        <div className="w-[100%]  md:w-[30%]">
-          <h2>Reading Time:</h2>
-          <h2>Bookmark Count:</h2>
+        <div className="w-[100%]  md:w-[30%] p-[10px]">
+          <h2 className="font-bold text-[24px]">Reading Time:</h2>
+          <h2 className="font-bold text-[24px]">Bookmark Count:</h2>
         </div>
       </div>
     </>
--- src/components/blog.jsx
+++ src/components/blog.jsx
@@ -1,28 +1,45 @@
+import { FcBookmark } from "react-icons/fc";
 const Blog = ({ datahere }) => {
-  console.log(datahere);
-  return <div className="grid grid-cols-1 md:grid-cols-2  gap-[20px]">
-    {
-        datahere.map((el)=>(
-<div className="card bg-base-100 w-fit shadow-sm flex flex-col gap-[5px] h-96 border-2 rounded-xl p-[10px]">
-  <div className="card-body">
-    <h2 className="card-title font-bold text-[32px]">{el.title}</h2>
-   
-  </div>
-   <figure className="h-[200px] w-full overflow-hidden rounded-xl">
-    <img
-      className="w-full h-full object-cover"
-      src={el.cover}
-      alt={el.title}
-    />
-  </figure>
-      <div className="card-actions ">
-      <button className="btn btn-primary bg-blue-400 p-[10px] rounded-xl font-bold text-[16px] " >Mark As Read</button>
+  
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-2  gap-[20px]">
+      {datahere.map((el) => (
+        <div  className="card bg-base-100 w-fit shadow-sm flex flex-col gap-[5px] h-96 border-2 rounded-xl p-[10px]">
+          <div className="card-body">
+            <h2 className="card-title font-bold text-[32px]">{el.title}</h2>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex gap-[15px]">
+              <h2 className="text-[16px]">
+                <span className="font-bold">Auther Name: </span>
+                {el.author}
+              </h2>
+              <img className="w-[24px] h-fit" src={el.author_img} alt="" />
+            </div>
+            <div>
+              <h2 className="font-bold">
+                {
+                  el.hashtags.map((item)=><span className="mr-[5px]"> #{item}</span>)
+                }
+              </h2>
+            </div>
+          </div>
+          <figure className="h-[200px] w-full overflow-hidden rounded-xl">
+            <img
+              className="w-full h-full object-cover"
+              src={el.cover}
+              alt={el.title}
+            />
+          </figure>
+          <div className="card-actions flex items-center justify-between mt-[10px]">
+            <button className="btn btn-primary bg-blue-400 p-[10px] rounded-xl font-bold text-[16px] ">
+              Mark As Read
+            </button>
+            <div className="bookmark mr-[5px] w-"><FcBookmark size={25} /></div>
+          </div>
+        </div>
+      ))}
     </div>
-
-</div>
-        ))
-    }
-    
-  </div>;
+  );
 };
 export default Blog;
